ae4695fb96813f5cac4c3a6d379db4aaa401c000
Chayut Teeraratkul <cteerara@u.rochester.edu>
2018-10-01 16:49:20 -0400

    Implemented integration for position. Generalized RHS function input

--- Lagevin/LagIntegrator.py
+++ Lagevin/LagIntegrator.py
@@ -2,18 +2,32 @@ import LagInput
 import LagIO
 
 
-def RK4(RHS, lagin, F0, t):
-  k1 = RHS(F0,t)
+def RK4(RHS, lagin, F):
+  # INPUT:  Function RHS
+  #         LagInput lagin
+  #         tuple F
+  # Takes the Right-Hand_side of an equation \frac{dx}{dt} = RHS(x,t) which the function will integrate. 
+  # Step size is defined in lagin under dt.
+  # F is the input to the RHS function. Note that the first argument of RHS MUST always be the initial condition of the function you want to integrate.
+  F0 = F[0]
+  F_list = list(F)
+
+  k1 = RHS(*F)
   F1 = F0 + k1*lagin.dt/2.0
-  
-  k2 = RHS(F1,t)
+  F_list[0] = F1
+  F = tuple(F_list)  
+
+  k2 = RHS(*F)
   F2 = F0 + k2*lagin.dt/2.0
+  F_list[0] = F2
+  F = tuple(F_list)
 
-  k3 = RHS(F2,t)
+  k3 = RHS(*F)
   F3 = F0 + k3*lagin.dt
+  F_list[0] = F3
+  F = tuple(F_list) 
 
-  k4 = RHS(F3,t)
+  k4 = RHS(*F)
   Fn = F0 + (k1+2*k2+2*k3+k4)*lagin.dt/6.0
-
   return Fn
 
--- Lagevin/Lagevin.py
+++ Lagevin/Lagevin.py
@@ -1,17 +1,54 @@
-# -*- coding: utf-8 -*-
-#import LagInput
-#import LagIO
-#
-## Lagevin input
-#lin = LagIO.LagIO_read("Lag.in") 
+import LagInput
+import LagIO
+import LagRHS
+import random
+import LagIntegrator as LInt
+import numpy as np
+import matplotlib.pyplot as plt
+lagin = LagIO.LagIO_readInput("Lag.in")
 
-# Lagevin velocity equation
-def Lagevin_vel_RHS(lagin):
+# Defining velocity function
+def vel_RHS(v,t):
   # INPUT: LagInput lagin 
+  #        Double rand
+  #        Double v
   # OUTPUT: Double output
   # Function compute the Right-Hand-Side of the Lagevin velocity equation
   # \frac{dv(t)}{dt} = -\frac{\gamma}{m}v(t) + \frac{1}{m} \epsilon (t)
-
   
+  epsilon_var = 2*lagin.IniTemp*lagin.DampCoef
+  epsilon_std = np.sqrt(epsilon_var)
+  epsilon = np.random.normal(0,epsilon_std,1)
+  RHS = -lagin.DampCoef*v + epsilon
+  return RHS
+ 
+def pos_RHS(x0,v,t):
+  # \frac{dx(t)}{dt} = v(t)
+  return v
 
-"""Main module."""
+WallBound = 5
+
+#------------------------------------------------------
+# Main Loop
+V = [0] * lagin.ttot
+X = [0] * lagin.ttot
+V[0] = lagin.IniVel
+X[0] = lagin.IniPos
+print(V[0],X[0])
+
+tvec = np.linspace(0,lagin.ttot*lagin.dt,num=lagin.ttot)
+for i in range(1,lagin.ttot):
+  t = lagin.dt*i
+  if X[i-1] > 0 and X[i-1] < WallBound:  
+    V[i] = LInt.RK4(vel_RHS, lagin, (V[i-1],t))
+  else:
+    V[i] = 0
+  Vin = V[i]
+  X[i] = LInt.RK4(pos_RHS, lagin, (X[i-1],Vin,t))
+
+plt.subplot(2,1,1)
+plt.title("Position (top), Velocity (Bottom)")
+plt.plot(tvec,X)
+plt.subplot(2,1,2)
+plt.plot(tvec,V)
+plt.show() 
