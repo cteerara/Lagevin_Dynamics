0d143d622267cdf0c6775d816a122b38f639b101
Chayut Teeraratkul <cteerara@u.rochester.edu>
2018-10-01 14:49:59 -0400

    Added integrated velocity

--- Lagevin/LagIntegrator.py
+++ Lagevin/LagIntegrator.py
@@ -2,17 +2,17 @@ import LagInput
 import LagIO
 
 
-def RK4(RHS, lagin, F0):
-  k1 = RHS(F0)
+def RK4(RHS, lagin, F0, t):
+  k1 = RHS(F0,t)
   F1 = F0 + k1*lagin.dt/2.0
   
-  k2 = RHS(F1)
+  k2 = RHS(F1,t)
   F2 = F0 + k2*lagin.dt/2.0
 
-  k3 = RHS(F2)
+  k3 = RHS(F2,t)
   F3 = F0 + k3*lagin.dt
 
-  k4 = RHS(F3)
+  k4 = RHS(F3,t)
   Fn = F0 + (k1+2*k2+2*k3+k4)*lagin.dt/6.0
 
   return Fn
--- Lagevin/Main.py
+++ Lagevin/Main.py
@@ -3,18 +3,36 @@ import LagIO
 import LagRHS
 import random
 import LagIntegrator as LInt
+import numpy as np
+import matplotlib.pyplot as plt
+lagin = LagIO.LagIO_readInput("Lag.in")
 
-lin = LagIO.LagIO_read("Lag.in")
-def printLagin():
-	print(lin.IniVel)
-#v = lin.IniVel
-#x0 = lin.IniPos
-#for i in range(0,lin.ttot):
-#
-#  rand = random.uniform(0,1)
-#  v = Integrator.RK4(lin, rand, v, "vel")
-#  x = Integrator.RK4(lin, rand, v, "pos")
-#  print(x)
+# Defining velocity function
+def vel_RHS(v,t):
+  # INPUT: LagInput lagin 
+  #        Double rand
+  #        Double v
+  # OUTPUT: Double output
+  # Function compute the Right-Hand-Side of the Lagevin velocity equation
+  # \frac{dv(t)}{dt} = -\frac{\gamma}{m}v(t) + \frac{1}{m} \epsilon (t)
   
-  
+  epsilon_var = 2*lagin.IniTemp*lagin.DampCoef
+  epsilon_std = np.sqrt(epsilon_var)
+  epsilon = np.random.normal(0,epsilon_std,1)
+  RHS = -lagin.DampCoef*v + epsilon
+  return RHS
+ 
+ 
+V0 = lagin.IniVel
+V = V0
 
+#------------------------------------------------------
+# Main Loop
+V_vec = np.linspace(0.0,0.0,num=lagin.ttot)
+for i in range(0,int(lagin.ttot)):
+  t = lagin.dt*i
+  V = LInt.RK4(vel_RHS, lagin, V,t)
+  V_vec[i] = V
+
+plt.plot(V_vec)
+plt.show() 
