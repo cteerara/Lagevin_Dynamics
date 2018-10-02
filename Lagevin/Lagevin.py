93bd7251eedbbe2be2600b1ff324e36182cfd079
Chayut Teeraratkul <cteerara@u.rochester.edu>
2018-10-01 20:38:19 -0400

    Implemented histogram plot

--- Lagevin/Lagevin.py
+++ Lagevin/Lagevin.py
@@ -34,21 +34,53 @@ V = [0] * lagin.ttot
 X = [0] * lagin.ttot
 V[0] = lagin.IniVel
 X[0] = lagin.IniPos
-print(V[0],X[0])
 
+NumOfRun = 100
+TimeHitWall = [0] * NumOfRun
 tvec = np.linspace(0,lagin.ttot*lagin.dt,num=lagin.ttot)
-for i in range(1,lagin.ttot):
-  t = lagin.dt*i
-  if X[i-1] > 0 and X[i-1] < WallBound:  
-    V[i] = LInt.RK4(vel_RHS, lagin, (V[i-1],t))
-  else:
-    V[i] = 0
-  Vin = V[i]
-  X[i] = LInt.RK4(pos_RHS, lagin, (X[i-1],Vin,t))
 
-plt.subplot(2,1,1)
-plt.title("Position (top), Velocity (Bottom)")
-plt.plot(tvec,X)
-plt.subplot(2,1,2)
-plt.plot(tvec,V)
-plt.show() 
+for j in range(0,NumOfRun):
+  for i in range(1,lagin.ttot):
+    t = lagin.dt*i
+    if X[i-1] >= 0 and X[i-1] <= WallBound:  
+      V[i] = LInt.RK4(vel_RHS, lagin, (V[i-1],t))
+    else: # If particle hit the wall.
+      V[i] = 0
+      TimeHitWall[j] = t
+      break
+    Vin = V[i]
+    X[i] = LInt.RK4(pos_RHS, lagin, (X[i-1],Vin,t))
+  
+
+  plt.figure(200)
+  plt.plot(tvec[0:i-1],X[0:i-1])
+  
+  plt.figure(300)
+  plt.plot(tvec[0:i-1],V[0:i-1])
+
+
+
+plt.figure(200)
+plt.title('Position')
+plt.xlabel('Time')
+plt.ylabel('Position')
+
+plt.figure(300)
+plt.title('Velocity')
+plt.xlabel('Time')
+plt.ylabel('Velocity')
+
+n_bins = 20
+plt.figure(100)
+plt.title('Time until particle hit the wall')
+plt.hist(TimeHitWall, bins=n_bins)
+
+
+plt.show()
+
+#plt.figure(300)
+#plt.title("Velocity")
+#plt.show() 
+#plt.figure(200)
+#plt.title("Position")
+#plt.show()
